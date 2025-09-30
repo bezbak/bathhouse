8158123c8b2a7566cc3d208c333f68714c4030a2
bezbak <Cemetey.2019@gmail.com>
2025-10-01 02:11:01 +0600

    fsaef

--- core/settings.py
+++ core/settings.py
@@ -112,7 +112,11 @@ STATIC_URL = 'static/'
 #STATICFILES_DIRS = [
 #    os.path.join(BASE_DIR, 'staticfiles'),
 #]
+<<<<<<< HEAD
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+=======
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+>>>>>>> adb5e92 (get commit)
 STATIC_URL = 'static/'
 
 MEDIA_URL = 'media/'
